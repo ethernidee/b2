68c5f43082737a51b788561e0bcbcd5c9a6a7257
Berserker <noemail@noemail.com>
2014-06-13 21:19:18 +0400

    Added IFormattedOutput and comfortable wrapper to write formatted files

--- FilesEx.pas
+++ FilesEx.pas
@@ -5,21 +5,132 @@ AUTHOR:       Alexander Shostak (aka Berserker aka EtherniDee aka BerSoft)
 }
 
 (***)  interface  (***)
-uses SysUtils, Utils, Files, DataLib;
+uses SysUtils, Utils, Files, DataLib, StrLib;
 
 type
   (* IMPORT *)
   TStrList = DataLib.TStrList;
   TDict = DataLib.TDict;
+
+  (*
+  Provides means for formatted output to external storage.
+  It's recommended to internally use StrBuilder for file output.
+  Default indentation must be '  ', line end marker - #13#10.
+  *)
+  IFormattedOutput = interface
+    procedure SetIndentation (const Identation: string);
+    procedure SetLineEndMarker (const aLineEndMarker: string);
+
+    // Increase or decrease indentation
+    procedure Indent;
+    procedure Unindent;
+    
+    procedure Write (const Str: string);
+    // Same as Write + Write([line end marker])
+    procedure RawLine (const Str: string);
+    // Same as Write([indentation]) * [indent level] + RawLine(Str)
+    procedure Line (const Str: string);
+
+    property Indentation:   string write SetIndentation;
+    property LineEndMarker: string write SetLineEndMarker;
+  end; // .interface IFormattedOutput
+
+  TFileFormattedOutput = class (TInterfacedObject, IFormattedOutput)
+   protected
+    {O} fOutputBuf:     StrLib.TStrBuilder;
+        fFilePath:      string;
+        fIndentation:   string;
+        fIndentLevel:   integer;
+        fLineEndMarker: string;
+
+   public
+    constructor Create (const aFilePath: string);
+    destructor Destroy; override;
+
+    procedure SetIndentation (const aIndentation: string);
+    procedure SetLineEndMarker (const aLineEndMarker: string);
+
+    // Increase or decrease indentation
+    procedure Indent;
+    procedure Unindent;
+    
+    procedure Write (const Str: string);
+    // Same as Write + Write([line end marker])
+    procedure RawLine (const Str: string);
+    // Same as Write([indentation]) * [indent level] + RawLine(Str)
+    procedure Line (const Str: string);
+  end; // .class TFileFormattedOutput
   
 
 // List does not own its objects. It simply contains case insensitive file names
 function  GetFileList (const MaskedPath: string; SearchSubj: TSearchSubj): {O} TStrList;
 procedure MergeFileLists (MainList, DependantList: TStrList);
+function  WriteFormattedOutput (const FilePath: string): IFormattedOutput;
 
 
 (***) implementation (***)
 
+
+constructor TFileFormattedOutput.Create (const aFilePath: string);
+begin
+  fFilePath      := aFilePath;
+  fOutputBuf     := StrLib.TStrBuilder.Create;
+  fIndentation   := '  ';
+  fIndentLevel   := 0;
+  fLineEndMarker := #13#10;
+end; // .constructor TFileFormattedOutput.Create
+
+destructor TFileFormattedOutput.Destroy;
+begin
+  Files.WriteFileContents(fOutputBuf.BuildStr, fFilePath);
+  FreeAndNil(fOutputBuf);
+end; // .destructor TFileFormattedOutput.Destroy
+
+procedure TFileFormattedOutput.SetIndentation (const aIndentation: string);
+begin
+  fIndentation := aIndentation;
+end; // .procedure TFileFormattedOutput.SetIndentation
+
+procedure TFileFormattedOutput.SetLineEndMarker (const aLineEndMarker: string);
+begin
+  fLineEndMarker := aLineEndMarker;
+end; // .procedure TFileFormattedOutput.SetLineEndMarker
+
+procedure TFileFormattedOutput.Indent;
+begin
+  Inc(fIndentLevel);
+end; // .procedure TFileFormattedOutput.Indent
+
+procedure TFileFormattedOutput.Unindent;
+begin
+  if fIndentLevel > 0 then begin
+    Dec(fIndentLevel);
+  end; // .if
+end; // .procedure TFileFormattedOutput.Unindent
+
+procedure TFileFormattedOutput.Write (const Str: string);
+begin
+  fOutputBuf.Append(Str);
+end; // .procedure TFileFormattedOutput.Write
+
+procedure TFileFormattedOutput.RawLine (const Str: string);
+begin
+  fOutputBuf.Append(Str);
+  fOutputBuf.Append(fLineEndMarker);
+end; // .procedure TFileFormattedOutput.RawLine
+
+procedure TFileFormattedOutput.Line (const Str: string);
+var
+  i: integer;
+
+begin
+  for i := 1 to fIndentLevel do begin
+    fOutputBuf.Append(fIndentation);
+  end; // .for
+
+  fOutputBuf.Append(Str);
+  fOutputBuf.Append(fLineEndMarker);
+end; // .procedure TFileFormattedOutput.Line
 
 function GetFileList (const MaskedPath: string; SearchSubj: TSearchSubj): {O} TStrList;
 begin
@@ -55,5 +166,15 @@ begin
   // * * * * * //
   SysUtils.FreeAndNil(NamesDict);
 end; // .procedure MergeFileLists
+
+function WriteFormattedOutput (const FilePath: string): IFormattedOutput;
+var
+{O} FormattedOutputObj: TFileFormattedOutput;
+
+begin
+  FormattedOutputObj := TFileFormattedOutput.Create(FilePath);
+  // * * * * * //
+  result := FormattedOutputObj; FormattedOutputObj := nil;
+end; // .function WriteFormattedOutput
 
 end.
