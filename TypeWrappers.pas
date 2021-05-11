97ffa64b367e504168e061bab53f9d60e8c22ca6
Berserker <noemail@noemail.com>
2021-05-11 04:38:46 +0300

    Added TInt wrapper and default value support

--- TypeWrappers.pas
+++ TypeWrappers.pas
@@ -10,28 +10,37 @@ uses Utils;
 type
   TString = class (Utils.TCloneable)
     Value: string;
-    
+
     constructor Create (const Value: string);
     procedure Assign (Source: Utils.TCloneable); override;
 
     class function ToPchar ({n} Str: TString): pchar; static;
-    class function ToString ({n} Str: TString): string; static;
-  end; // .class TString
+    class function ToString ({n} Str: TString; const DefValue: string = ''): string; static;
+  end;
+
+  TInt = class (Utils.TCloneable)
+    Value: integer;
+
+    constructor Create (Value: integer);
+    procedure Assign (Source: Utils.TCloneable); override;
+
+    class function ToInteger ({n} Wrapper: TInt; DefValue: integer = 0): integer; static;
+  end;
 
   TWideString = class (Utils.TCloneable)
     Value: WideString;
-    
+
     constructor Create (const Value: WideString);
     procedure Assign (Source: Utils.TCloneable); override;
   end;
 
   TEventHandler = class (Utils.TCloneable)
     Handler:  Utils.TEventHandler;
-    
+
     constructor Create (Handler: Utils.TEventHandler);
     procedure Assign (Source: Utils.TCloneable); override;
-  end; // .class TEventHandler
-  
+  end;
+
 
 (***) implementation (***)
 
@@ -55,12 +64,31 @@ begin
   end;
 end;
 
-class function TString.ToString ({n} Str: TString): string;
+class function TString.ToString ({n} Str: TString; const DefValue: string = ''): string;
 begin
   if Str = nil then begin
-    result := '';
+    result := DefValue;
   end else begin
     result := Str.Value;
+  end;
+end;
+
+constructor TInt.Create (Value: integer);
+begin
+  Self.Value := Value;
+end;
+
+procedure TInt.Assign (Source: Utils.TCloneable);
+begin
+  Self.Value := (Source AS TInt).Value;
+end;
+
+class function TInt.ToInteger ({n} Wrapper: TInt; DefValue: integer = 0): integer;
+begin
+  if Wrapper = nil then begin
+    result := DefValue;
+  end else begin
+    result := Wrapper.Value;
   end;
 end;
 
